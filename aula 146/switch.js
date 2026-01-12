04b09e4f6f2db77e11e53ac490cd0f7da7fc7d89
Enzo Ryan Simette <enzoryansimetteplr@gmail.com>
2026-01-12 09:27:51 -0300

    switch

---
+++ aula 146/switch.js
@@ -0,0 +1,13 @@
+// Alternativa ao If... Else if... Else
+let valor = 1
+switch (valor) {
+    case 1:
+        // código para valor 1
+        break
+    case 2:
+        // código para valor 2
+        break
+    default:  // Se NENHUM for verdadeiro, ele exibe esse resultado
+        //código para valor 3
+        break
+}
