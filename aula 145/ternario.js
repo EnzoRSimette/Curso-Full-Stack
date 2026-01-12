28229b8a74e8311ce5b0c4a008c1d3632606b946
Enzo Ryan Simette <enzoryansimetteplr@gmail.com>
2026-01-12 09:14:46 -0300

    Operador ternario

---
+++ aula 145/ternario.js
@@ -0,0 +1,12 @@
+/*
+
+var resultado = (condição) ? valor1 : valor2 
+? Pergunta se é verdadeiro
+Se for, valor 1
+Se não for, valor 2
+
+*/
+
+let enviar_email = false
+let frase = enviar_email ? "enviar" : "não enviar"
+console.log("frase")
